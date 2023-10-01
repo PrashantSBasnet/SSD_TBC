c0b28ece24eda02f1ef124a4b9a251a8185f3f9f
Prashant Singh Basnet <prashantsingh.basnet@cotiviti.com>
2023-10-01 16:03:52 +0545

    w3

--- W3Eg/javaDoc/doc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"javaDocSample","c":"Driver","l":"add(int, int)","u":"add(int,int)"},{"p":"javaDocSample","c":"Driver","l":"Driver()","u":"%3Cinit%3E()"},{"p":"javaDocSample","c":"Driver","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
