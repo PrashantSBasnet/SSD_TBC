bef7e0962f43eacf504c7543a6d4288a43524003
Prashant Singh Basnet <prasbas97@gmail.com>
2023-10-07 16:13:58 +0545

    Queue

---
+++ W6/CollectionFramework/src/queueInterface/PriorityQueueDemo.java
@@ -0,0 +1,24 @@
+package queueInterface;
+
+import java.util.PriorityQueue;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class PriorityQueueDemo {
+
+	public static void main(String[] args) {
+		
+		PriorityQueue pq = new PriorityQueue();
+	
+		PriorityQueue pq2 = new PriorityQueue(20);
+	
+		//PriorityQueue pq3 = new PriorityQueue(int initialCapacity, Comparator c);
+
+		SortedSet s = new TreeSet();
+		//PriorityQueue pq4 = new PriorityQueue(SortedSet s);
+		
+		//List l = new ArrayList();
+		//PriorityQueue pq4 = new PriorityQueue(l);
+		
+	}
+}
---
+++ W6/CollectionFramework/src/queueInterface/QueueDemo.java
@@ -0,0 +1,31 @@
+package queueInterface;
+
+public class QueueDemo {
+
+	public static void main(String[] args) {
+		
+		//boolean add(E e);
+		//add elements at the tail of the queue. for Priority queue, as per priority
+		
+		//boolean offer(E e);
+		//adding elements at the tail of the queue. for Priority queue, as per priority
+		
+		//E remove();
+		//removes element and returns head of the queue. throws NoSuchElementException if queue is empty 
+		
+		//E poll();
+		//removes and returns head of the queue. Null for empty queue
+		
+		//E element();
+		//view the head of the queue without removing. throws NoSuchElementException if queue is empty 
+		
+		//E peek();	
+		//view the head of the queue without removing. null if queue is empty 
+
+		
+		
+	
+
+	}
+
+}
