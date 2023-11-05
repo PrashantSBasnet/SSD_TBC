81e3f5dacc7db00a5ec1a5929b9b10a4bea9cac1
Prashant Singh Basnet <prasbas97@gmail.com>
2023-11-05 21:06:15 +0545

    exception codes and notes updated

--- W4/exceptionHandling/src/exceptionHandling/FileNotFoundExceptionExample.java
+++ W4/exceptionHandling/src/exceptionHandling/FileNotFoundExceptionExample.java
@@ -14,20 +14,25 @@ import java.io.IOException;
 public class FileNotFoundExceptionExample {
 
 	public static void fileNotFoundException(){
+		
 		String filePath = "nonexistentfile.txt";
+		
 		try {
 			FileReader fileReader = new FileReader(filePath);
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
 
 			String line;
+			
 			while ((line = bufferedReader.readLine()) != null) {
 				System.out.println(line);
 			}
 			bufferedReader.close();
+			
 		} catch (FileNotFoundException e) { //more specific exception
 			// Handle the FileNotFoundException
 			System.err.println("The file " + filePath + " was not found."); //this message gets generated
-		} catch (IOException e) { //will be caught at last
+			
+		} catch (IOException e) { 
 			// Handle other IOExceptions
 			System.err.println("An IOException occurred: " + e.getMessage());
 		}
